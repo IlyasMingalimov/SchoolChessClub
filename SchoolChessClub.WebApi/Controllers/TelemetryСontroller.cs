9ead006ad95ab2e0113bb6264135bf8bdf577339
IlyasMingalimov <neugolok@gmail.com>
2025-05-28 17:58:34 +0500

    Add TelemetryСontroller to web api

---
+++ SchoolChessClub.WebApi/Controllers/TelemetryСontroller.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace SchoolChessClub.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TelemetryСontroller : ControllerBase
+    {
+        [HttpGet("Ping")]
+        [AllowAnonymous]
+        public IActionResult Ping() 
+        {
+            return new ContentResult
+            {
+                ContentType = "text/html",
+                Content = $"Current time UTC {DateTime.UtcNow.ToString(CultureInfo.InvariantCulture)}"
+            };
+        }
+    }
+}
---
+++ SchoolChessClub.WebApi/Program.cs
@@ -0,0 +1,25 @@
+namespace SchoolChessClub.WebApi;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+        app.MapControllers();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.Run();
+    }
+}
