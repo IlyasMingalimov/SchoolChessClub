8c5cbd2ee451b7e236cc1c92733c9f2867b6292b
IlyasMingalimov <neugolok@gmail.com>
2025-06-03 09:53:06 +0500

    fix swagger

--- SchoolChessClub.WebApi/Program.cs
+++ SchoolChessClub.WebApi/Program.cs
@@ -18,12 +18,14 @@ public class Program
         var app = builder.Build();
         app.MapControllers();
 
-        if (app.Environment.IsDevelopment())
-        {
+        //if (app.Environment.IsDevelopment())
+        //{
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;});
+        //}
 
         app.Run();
     }
